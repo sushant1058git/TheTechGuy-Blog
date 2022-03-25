1941cbbf00f1ec98d88b6c7b8bf6fb1d5ec37d94
Sushant.sinha_94 <sushant1058jan@gmail.com>
2022-03-25 23:44:29 +0530

    changes made

--- aboutme/models.py
+++ aboutme/models.py
@@ -1,9 +1,12 @@
 import email
+from re import T
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
 class AboutMe(models.Model):
-    about=models.TextField(max_length=500, null=True,blank=True)
+    about=RichTextField(null=True,blank=True)
     name=models.CharField(max_length=30)
     dob=models.DateField()
     school=models.CharField(max_length=50,blank=True,null=True)
@@ -17,21 +20,14 @@ class AboutMe(models.Model):
     
     
 class Skills(models.Model):
-    languages=models.CharField(max_length=50,null=True,blank=True)
-    webdesign=models.CharField(max_length=50,null=True,blank=+True)
-    libraries=models.CharField(max_length=50,null=True,blank=+True)
-    databases=models.CharField(max_length=50,null=True,blank=+True)
-    framework=models.CharField(max_length=50,null=True,blank=+True)
-    ide=models.CharField(max_length=50,null=True,blank=+True)
-    versioncontrol=models.CharField(max_length=50,null=True,blank=+True)
-    
+   skill=models.CharField(max_length=250,null=True,blank=True)
     
 class WorkExp(models.Model):
     comapny_name=models.CharField(max_length=50,null=True,blank=True)
     join_date=models.DateField(null=True,blank=True)
     end_date=models.DateField(null=True, blank=True)
     project_title=models.CharField(max_length=100)
-    project_details=models.TextField(max_length=200,null=True, blank=True)
+    project_details=RichTextField(null=True, blank=True)
     designation=models.CharField(max_length=50)
     
     def __str__(self):
@@ -61,4 +57,5 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.email
+    
         
